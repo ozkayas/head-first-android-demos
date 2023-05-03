c44d132d4572aed6e33581ed85b0799619583569
Suat Özkaya <suatozkaya@gmail.com>
2023-05-03 18:10:49 +0300

    Finish chapter : 9

---
+++ BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/OrderFragment.kt
@@ -0,0 +1,93 @@
+package com.hfad.bitsandpizzas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.chip.Chip
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [OrderFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class OrderFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_order, container, false)
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
+            val pizzaType = pizzaGroup.checkedRadioButtonId
+            if (pizzaType == -1) {
+                val text = "Please select a pizza type"
+                Toast.makeText(activity, text, Toast.LENGTH_LONG).show()
+            } else {
+                var text = (when (pizzaType) {
+                    R.id.radio_diavolo -> "Diavolo pizza"
+                    R.id.radio_funghi -> "Funghi pizza"
+                    else -> ""
+                })
+
+                val parmesan = view.findViewById<Chip>(R.id.parmesan)
+                val chiliOil = view.findViewById<Chip>(R.id.chili_oil)
+                text += if (parmesan.isChecked) " with parmesan" else ""
+                text += if (chiliOil.isChecked) " with parmesan" else ""
+
+                Snackbar.make(fab, text, Snackbar.LENGTH_LONG).show()
+
+            }
+        }
+        return view
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment OrderFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            OrderFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
